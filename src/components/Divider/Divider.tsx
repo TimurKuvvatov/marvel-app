ffb8203b3a606cd8216e08dfdbdabfceef3b6b48
TimurKuvvatov <tkuvvatov@gmail.com>
2024-10-28 21:26:40 +0600

    [LAB-1] Add Divider component

---
+++ src/components/Divider/Divider.tsx
@@ -0,0 +1,9 @@
+import React, { FC } from 'react'
+import classes from './Divider.module.scss'
+const Divider: FC = () => {
+  return (
+    <div className={classes.divider}></div>
+  )
+}
+
+export default Divider
