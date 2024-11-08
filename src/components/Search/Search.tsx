22d84349c6084549530f15fe01b55b8cb8a84f99
TimurKuvvatov <tkuvvatov@gmail.com>
2024-11-08 22:55:22 +0600

    [LAB-2] Update Search component to implement debounce logic

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,16 +1,36 @@
-import React, { FC } from 'react'
-import classes from './Search.module.scss'
+import React, { FC, useState } from 'react';
+import classes from './Search.module.scss';
+import useDebounce from '../../hooks/useDebounce';
+import PostsStore from '../../stores/PostsStore';
 
 interface SearchProps {
-  placeholder: string
-}
-const Search: FC<SearchProps> = ({placeholder}) => {
-  return (
-    <div className={classes.search}>
-        <input type="text" placeholder={`Search for ${placeholder} by Name`} className={classes.input} />
-        <button className={classes.button}>SEARCH</button>
-    </div>
-  )
+  placeholder: string;
+  type: 'character' | 'comic';
 }
 
-export default Search
+const Search: FC<SearchProps> = ({ placeholder, type }) => {
+  const [searchTerm, setSearchTerm] = useState('');
+  const debouncedSearchTerm = useDebounce(searchTerm, 300);
+
+  const handleSearch = () => {
+    if (type === 'character') {
+      PostsStore.fetchCharacters(0, debouncedSearchTerm);
+    } else {
+      PostsStore.fetchComics(0, debouncedSearchTerm);
+    }
+  };
+
+  return (
+    <div className={classes.search}>
+      <input 
+        type="text" 
+        placeholder={`Search for ${placeholder} by Name`} 
+        className={classes.input} 
+        onChange={(e) => setSearchTerm(e.target.value)} 
+      />
+      <button className={classes.button} onClick={handleSearch}>SEARCH</button>
+    </div>
+  );
+};
+
+export default Search;
