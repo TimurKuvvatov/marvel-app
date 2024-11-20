ae508b3c7ea3b2a5807a047158fce5005afd980e
TimurKuvvatov <tkuvvatov@gmail.com>
2024-11-20 22:53:02 +0600

    [LAB-3] Create and implement FavoritesPage component

---
+++ src/pages/FavoritesPage.tsx
@@ -0,0 +1,40 @@
+import React, { FC } from 'react';
+import PageTitle from '../components/PageTitle/PageTitle';
+import Divider from '../components/Divider/Divider';
+import CardSection from '../components/CardSection/CardSection';
+import useLocalStorage from '../hooks/useLocalStorage';
+import { Character, Comic } from '../types/dataTypes';
+
+const FavoritesPage: FC = () => {
+  const [favCharacters, setFavCharacters] = useLocalStorage<Character[]>(
+    'favorites-characters',
+    [],
+  );
+  const [favComics, setFavComics] = useLocalStorage<Comic[]>('favorites-comics', []);
+  return (
+    <div className="container">
+      <PageTitle title="Favorites" subtitle="count" />
+      <Divider />
+      <section>
+        <h2>Characters</h2>
+        {favCharacters.length !== 0 ? (
+          <CardSection items={favCharacters} pageName="comics" />
+        ) : (
+          <div>You don't have favorites characters</div>
+        )}
+      </section>
+      <Divider />
+      <section>
+        <h2>Comics</h2>
+        {favComics.length !== 0 ? (
+          <CardSection items={favComics} pageName="comics" />
+        ) : (
+          <div>You don't have favorites comics</div>
+        )}
+      </section>  
+      <Divider />
+    </div>
+  );
+};
+
+export default FavoritesPage;
