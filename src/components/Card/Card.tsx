8ec31de83e4cf1fd0a0ba27b36167f76078d873d
TimurKuvvatov <tkuvvatov@gmail.com>
2024-11-21 00:45:39 +0600

    [LAB-3] Added favorite heart icon in cards, used useLocalStorage to save favorites

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -4,18 +4,32 @@ import classes from './Card.module.scss';
 
 interface CardProps {
   item: Character | Comic;
+  isFavorite: boolean;
   onClick: (item: Character | Comic) => void;
+  onToggleFavorite: (item: Character | Comic) => void;
 }
 
-const Card: FC<CardProps> = ({ item, onClick }) => {
+const Card: FC<CardProps> = ({ item, isFavorite, onClick, onToggleFavorite }) => {
   const imageUrl = `${item.thumbnail.path}.${item.thumbnail.extension}`;
 
   return (
     <div onClick={() => onClick(item)} className={classes.card}>
-      <div className={classes.image} style={{ background: `url(${imageUrl})` }}></div>
+      <div
+        className={`${classes.heart} ${isFavorite ? classes.isFavorite : ''}`}
+        onClick={(e) => {
+          e.stopPropagation();
+          onToggleFavorite(item);
+        }}
+      ></div>
+      <div
+        className={classes.image}
+        style={{ backgroundImage: `url(${imageUrl})`, backgroundSize: 'cover' }}
+      />
       <div className={classes.content}>
         <h3 className={classes.title}>{'name' in item ? item.name : item.title}</h3>
-        <p className={classes.description}>{item.description ? item.description : "No description provided"}</p>
+        <p className={classes.description}>
+          {item.description ? item.description : 'No description provided'}
+        </p>
       </div>
     </div>
   );
--- src/components/CardSection/CardSection.tsx
+++ src/components/CardSection/CardSection.tsx
@@ -3,17 +3,58 @@ import { Character, Comic } from '../../types/dataTypes';
 import classes from './CardSection.module.scss';
 import Card from '../Card/Card';
 import { useNavigate } from 'react-router-dom';
+import useLocalStorage from '../../hooks/useLocalStorage';
+
 interface CardSectionProps {
   items: Character[] | Comic[];
   pageName: string;
 }
+
 const CardSection: FC<CardSectionProps> = ({ items, pageName }) => {
   const navigate = useNavigate();
+
+  const [favCharacters, setFavCharacters] = useLocalStorage<Character[]>(
+    `favorites-characters`,
+    [],
+  );
+  const [favComics, setFavComics] = useLocalStorage<Comic[]>(`favorites-comics`, []);
+
+  const toggleFavorite = (item: Character | Comic) => {
+    if ('name' in item) {
+      const isFavorite = favCharacters.some((fav) => fav.id === item.id);
+      const newFavorites = isFavorite
+        ? favCharacters.filter((fav) => fav.id !== item.id)
+        : [...favCharacters, item as Character];
+
+      setFavCharacters(newFavorites);
+    } else {
+      const isFavorite = favComics.some((fav) => fav.id === item.id);
+      const newFavorites = isFavorite
+        ? favComics.filter((fav) => fav.id !== item.id)
+        : [...favComics, item as Comic];
+
+      setFavComics(newFavorites);
+    } 
+  };
+
   return (
     <div className={classes.cardSection}>
-      {items.map((item) => (
-        <Card key={`${pageName}-${item.id}`} onClick={() => navigate(`/${pageName}/${item.id}`)} item={item} />
-      ))}
+      {items.map((item) => {
+        const isFavorite =
+          'name' in item
+            ? favCharacters.some((fav) => fav.id === item.id)
+            : favComics.some((fav) => fav.id === item.id);
+
+        return (
+          <Card
+            key={`${pageName}-${item.id}`}
+            onClick={() => navigate(`/${pageName}/${item.id}`)}
+            item={item}
+            isFavorite={isFavorite}
+            onToggleFavorite={toggleFavorite}
+          />
+        );
+      })}
     </div>
   );
 };
