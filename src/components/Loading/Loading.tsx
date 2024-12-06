d1e12f73ce104ea51f0f95123d1f5048c55e7dfb
TimurKuvvatov <tkuvvatov@gmail.com>
2024-12-06 23:48:53 +0600

    [LAB-4] Simplified loading logic in Loading component

--- src/components/Loading/Loading.tsx
+++ src/components/Loading/Loading.tsx
@@ -1,30 +1,10 @@
-import { useEffect, useState } from 'react';
 import classes from './Loading.module.scss';
 
 const Loading = () => {
-  const [errorMessage, setErrorMessage] = useState('');
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setErrorMessage('Something went wrong, please try again later.');
-    }, 15000);
-
-    return () => clearTimeout(timer);
-  }, []);
-
   return (
     <div className={classes.container}>
-      {errorMessage ? (
-        <>
-          <h2>Sorry ;(</h2>
-          <p>{errorMessage}</p>
-        </>
-      ) : (
-        <>
-          <div className={classes.loader}></div>
-          <h2>Wait for it</h2>
-        </>
-      )}
+      <div className={classes.loader}></div>
+      <h2>Wait for it</h2>
     </div>
   );
 };
