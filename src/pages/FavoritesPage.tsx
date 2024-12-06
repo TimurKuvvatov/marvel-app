ac1822df183f7c26367b153c591421bbf9cd05a1
TimurKuvvatov <tkuvvatov@gmail.com>
2024-12-06 21:00:53 +0600

    [LAB-4] Decomposed FavoritesPage and integrated useFavorites hook

--- src/pages/FavoritesPage.tsx
+++ src/pages/FavoritesPage.tsx
@@ -1,31 +1,16 @@
 import { FC } from 'react';
+import useFavorites from '../hooks/useFavorites';
+
 import PageTitle from '../components/PageTitle/PageTitle';
-import useLocalStorage from '../hooks/useLocalStorage';
-import { Character, Comic } from '../types/dataTypes';
 import FavoritesSection from '../components/FavoritesSection/FavoritesSection';
 
 const FavoritesPage: FC = () => {
-  const [favCharacters, setFavCharacters] = useLocalStorage<Character[]>(
-    'favorites-characters',
-    [],
-  );
-  const [favComics, setFavComics] = useLocalStorage<Comic[]>('favorites-comics', []);
-
+  const { favCharacters, favComics } = useFavorites();
   return (
     <div className="container">
       <PageTitle title="Favorites" subtitle={`${favComics.length + favCharacters.length}`} />
-      <FavoritesSection
-        title="Characters"
-        items={favCharacters}
-        pageName="characters"
-        setFavCharacters={setFavCharacters}
-      />
-      <FavoritesSection
-        title="Comics"
-        items={favComics}
-        pageName="comics"
-        setFavComics={setFavComics}
-      />
+      <FavoritesSection title="Characters" items={favCharacters} pageName="characters" />
+      <FavoritesSection title="Comics" items={favComics} pageName="comics" />
     </div>
   );
 };
